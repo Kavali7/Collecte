15f44e4f483acba48040c7bb444a59c87079592a
Kavali7 <georgesbusiness54@.comm>
2025-11-14 14:46:28 +0100

    Fix offline overlay color API

--- lib/features/boutiques/presentation/widgets/offline_blocking_overlay.dart
+++ lib/features/boutiques/presentation/widgets/offline_blocking_overlay.dart
@@ -14,7 +14,7 @@ class OfflineBlockingOverlay extends StatelessWidget {
     return IgnorePointer(
       ignoring: false,
       child: Container(
-        color: Colors.black.withOpacity(0.6),
+        color: Colors.black.withValues(alpha: 0.6),
         child: Center(
           child: Container(
             constraints: const BoxConstraints(maxWidth: 320),
